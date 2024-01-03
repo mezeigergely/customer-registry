67a8fc4e032abef7c38dbbf316439d5e7a1331ec
Geris <geris@Administrator-MacBook-Pro.local>
2024-01-03 18:35:28 +0100

    added timegrid

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,17 @@
 import './bootstrap';
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid'
 
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new Calendar(calendarEl, {
-        plugins: [ dayGridPlugin ],
-        initialView: 'dayGridMonth',
+        plugins: [ dayGridPlugin, timeGridPlugin ],
+        initialView: 'timeGridWeek',
         headerToolbar: {
             left: 'prev,next',
             center: 'title',
-            right: 'dayGridMonth,dayGridWeek,dayGridDay'
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
         }
     });
     calendar.render();
