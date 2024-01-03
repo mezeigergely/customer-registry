608f77f14581a4de073d77398cfc00d179c827a6
Geris <geris@Administrator-MacBook-Pro.local>
2024-01-03 17:55:42 +0100

    set vite and day-week-month grid

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new Calendar(calendarEl, {
+        plugins: [ dayGridPlugin ],
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            left: 'prev,next',
+            center: 'title',
+            right: 'dayGridMonth,dayGridWeek,dayGridDay'
+        }
+    });
+    calendar.render();
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,7 @@
   <head>
     <meta charset='utf-8' />
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js'></script>
-    <script>
-
-      document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
-        });
-        calendar.render();
-      });
-
-    </script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
   <body>
     <div id='calendar'></div>
